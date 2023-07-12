fa57a12e568b9cf55ab19819ff2a33332a65fc19
n714 <45053630+n714@users.noreply.github.com>
2023-07-12 00:09:16 -0700

    Update database.bat

--- database.bat
+++ database.bat
@@ -1,11 +1,13 @@
 @echo off
 
-: 輸入你的資料庫路徑檔案度路徑上"\"改為雙斜線"\\"較優
+: Enter your database path file path "\" to double slash "\\" is better.
+: 输入你的资料库路径档案度路径上"\"改为双斜线"\\"会更好.
+: set DATABASE_DIR="\\\Server\Resource\AI\models"
 
-  set DATABASE_DIR="R:\\AI"
-: set DATABASE_DIR="\\Server\Resource\AI\models"
+set DATABASE_DIR="R:\\\server\AI"
 
-: 下方為所有基本模型資料夾位置
+:Below are all base model folder locations: 
+:下方为所有基本模型资料夹位置
 
 set DIR_ARG=--ckpt-dir %DATABASE_DIR%"\\Stable-diffusion"^
 	    --vae-dir %DATABASE_DIR%"\\vae"^
@@ -20,37 +22,20 @@ set DIR_ARG=--ckpt-dir %DATABASE_DIR%"\\Stable-diffusion"^
 	    --codeformer-models-path %DATABASE_DIR%"\\Codeformer"^
 	    --gfpgan-models-path %DATABASE_DIR%"\\GFPGAN"
 
-: 下方為config檔案路徑(config內保存基本設定檔)
+: Below is the config file path (the basic configuration file is saved in config)
+: 下方为config档案路径(config内保存基本设定档)
 
 set CONFIG_DIR_ARG=--ui-settings-file %DATABASE_DIR%"\\config\\config.json"^
                    --ui-config-file %DATABASE_DIR%"\\config\\ui-config.json"^
                    --styles-file %DATABASE_DIR%"\\config\\styles.csv"
 
 
-
 : 常用擴充
 
-: controlnet模型路徑
+: Controlnet model path
+: Controlnet模型路径
 set CN_DIR=--controlnet-dir %DATABASE_DIR%"\\controlnet"
 
-: LyCORIS模型路徑(如果有使用LyCORIS擴充)
-set LYCO_DIR=--lyco-dir %DATABASE_DIR%"\\LyCORIS"
-
-:使用說明
-:在webui-user.bat中加入call database.bat
-:並在set COMMANDLINE_ARGS= 加入需要的命令需要以%%包覆
-
-:範例如下(這是webui-user的內容範例)
-
-:@echo off
-
-:call database.bat
-:set PYTHON=
-:set GIT=
-:set VENV_DIR=
-:set COMMANDLINE_ARGS=--no-half-vae --xformers --opt-sdp-no-mem-attention --opt-sdp-attention %DIR_ARG%  %CONFIG_DIR_ARG% %CN_DIR%
-:call webui.bat
-
-
-
-
+: LyCORIS model path (if installed LyCORIS Extension)
+：LyCORIS 模型路徑（如果安裝了 LyCORIS Extension）
+: set LYCO_DIR=--lyco-dir %DATABASE_DIR%"\\LyCORIS"
