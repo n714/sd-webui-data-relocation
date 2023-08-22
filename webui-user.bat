8f23e3f400cb165862ff400409cd5dcaf99882ec
n714 <45053630+n714@users.noreply.github.com>
2023-08-22 12:49:46 -0700

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -5,7 +5,7 @@ set PYTHON=
 set GIT=
 set VENV_DIR=
 
-set COMMANDLINE_ARGS= --api --medvram --autolaunch --xformers --opt-sdp-no-mem-attention --opt-sdp-attention %DIR_ARG% %CONFIG_DIR_ARG% %CN_DIR% %LYCO_DIR%
+set COMMANDLINE_ARGS= --api --medvram --autolaunch --xformers --opt-sdp-no-mem-attention --opt-sdp-attention %DIR_ARG% %CONFIG_DIR_ARG% %CN_DIR%
 git pull
 
 call webui.bat
